9de35b9db446cd6584c51fc31ed921785e700ba3
cake17 <cake17@cake-websites.com>
2015-03-02 17:42:07 +0100

    simplify config in component

--- src/Controller/Component/RecaptchaComponent.php
+++ src/Controller/Component/RecaptchaComponent.php
@@ -33,9 +33,12 @@ class RecaptchaComponent extends Component
      */
     public function initialize(array $config)
     {
+        // Get controller obj
         $controller = $this->_registry->getController();
         // Add the helper on the fly
         $controller->helpers[] = 'Recaptcha.Recaptcha';
+        // Set config from config/recaptcha.php
+        $this->config(Configure::read('Recaptcha'));
     }
 
     /**
@@ -48,32 +51,22 @@ class RecaptchaComponent extends Component
     public function startup(Event $event)
     {
         $controller = $event->subject();
-
-        // Register API keys at https://www.google.com/recaptcha/admin
-        $siteKey = Configure::read('Recaptcha.siteKey');
-        $secret = Configure::read('Recaptcha.secret');
-        // reCAPTCHA supported 40+ languages listed here: https://developers.google.com/recaptcha/docs/language
-        $lang = Configure::read('Recaptcha.defaultLang');
-        // either light or dark
-        $theme = Configure::read('Recaptcha.defaultTheme');
-        // either image or audio
-        $type = Configure::read('Recaptcha.defaultType');
-
+        // get host
         $host = $controller->request->env("REMOTE_ADDR");
 
         // The response from reCAPTCHA
         $resp = null;
         // The error code from reCAPTCHA, if any
         $error = null;
-        $reCaptcha = new ReCaptcha($secret);
+        $reCaptcha = new ReCaptcha($this->config('secret'));
 
         // send siteKey, lang, theme and type from config to helper
-        $controller->helpers['Recaptcha.Recaptcha']['siteKey'] = $siteKey;
-        $controller->helpers['Recaptcha.Recaptcha']['lang'] = $lang;
-        $controller->helpers['Recaptcha.Recaptcha']['theme'] = $theme;
-        $controller->helpers['Recaptcha.Recaptcha']['type'] = $type;
+        $controller->helpers['Recaptcha.Recaptcha']['siteKey'] = $this->config('siteKey');
+        $controller->helpers['Recaptcha.Recaptcha']['lang'] = $this->config('defaultLang');
+        $controller->helpers['Recaptcha.Recaptcha']['theme'] = $this->config('defaultTheme');
+        $controller->helpers['Recaptcha.Recaptcha']['type'] = $this->config('defaultType');
 
-        if ($controller->request->is(['post', 'put'])) {
+        if ($controller->request->is(['post', 'put', 'patch'])) {
             // if Recaptcha is not checked
             if (isset($controller->request->data["g-recaptcha-response"]) && empty($controller->request->data["g-recaptcha-response"])) {
                 $controller->Flash->error(__d('recaptcha', 'Please check Recaptcha Box.'));
